1bafcdbd5dd94b77aae88996ebeacf3cbd2a468a
aminaaman <aminaaman07@gmail.com>
2017-01-26 22:00:54 +0600

    Complex

--- Program.cs
+++ Program.cs
@@ -3,71 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO; //library for using StreamReader and StreamWriter
 
 namespace Amina
 {
 	class Program
 	{
-		static void emptySpace(int level) //function that displays 2*spaces where you apply it 
+		class Complex 
 		{
-			for (int i = 0; i < level * 2; i++)
+			public int x;
+			public int y;
+			public Complex(int x, int y) //constructor that initializes info
 			{
-				Console.WriteLine(" ");
-			}
-		}
-		static void Ex2(string path, int level) //function that opens directories and files recursively
-		{
-			if (level > 2)
-				return; // outputing only with 2 empty space
-			/*
-			 TRY identifies a block of code for which 
-			 particular exceptions is activated
-			 
-			 CATCH catches an exception
-			 */
-			try
-			{
-
-				// Getting directories
-				DirectoryInfo directory = new DirectoryInfo(path);
-				DirectoryInfo[] directories = directory.GetDirectories();
-
-				//Getting files
-				FileInfo[] files = directory.GetFiles();
-				Console.WriteLine("Files:");
-				foreach (FileInfo file in files)
-				{
-					emptySpace(level); // getting names of files
-					Console.WriteLine(file.Name);
-				}
-
-				Console.WriteLine("Directories:");
-
-				foreach (DirectoryInfo dInfo in directories)
-				{
-					//getting the fullname
-					emptySpace(level);
-					Console.WriteLine(dInfo.Name);
-					Ex2(dInfo.FullName, level + 1); //applying func to open directories in directories
-
-				}
+				this.x = x;
+				this.y = y;
 			}
 
-			catch (Exception e)
+			public override string ToString() //function that outputs data as a string
 			{
-
+				return x + "/" + y;
 			}
-		}
-		public static void Main(string[] args)
-		{
-			Ex2(@"/users/Adela/documents", 0);
-			Console.ReadKey();
+
+			public static Complex operator +(Complex x, Complex y) //function for overloading +
+			{ 
+				Complex c = new Complex(x.x * y.y + y.x * x.y, x.y * y.y);
+				return c;
+			}
+			static void Main(string[] args)
+			{
+				Complex a = new Complex(1, 2);
+				Complex b = new Complex(2, 3);
+
+				Complex c = a + b;
+				Console.WriteLine(c);
+				Console.ReadKey();
+			}
+
 		}
 	}
 }
-			
-			
-		
-		
 		
